2daba8fa44301a18c9cc44a894d29ae438ca4e7c
Khang Nguyen <46115541+mkhangg@users.noreply.github.com>
2022-02-19 10:25:04 -0600

    Update speaker.ino

--- Speaker/speaker.ino
+++ Speaker/speaker.ino
@@ -1,18 +1,41 @@
-// Configure pin 3 for speaker 
-#define SPEAKER 3
-
-// Function initiating speaker
-void pinInit() {
-    pinMode(SPEAKER, OUTPUT);
-    digitalWrite(SPEAKER, LOW);
-}
-
-// Initailize speaker when Arduino runs
+int iStart = 0;
+int iEnd = 3;
+int BassTab[]={1911, 1702, 1516, 1431, 1275, 1136, 1012};
+ 
 void setup() {
     pinInit();
 }
 
-// Generate the sound iteratively
 void loop() {
-    tone(SPEAKER, 100, 500);
+    for(int note_index = 0; note_index < 7; note_index++) {
+        sound(note_index);
+        delay(500);
+    }
+}
+void pinInit() {
+    for(int i = iStart; i <= iEnd; i++) {
+       pinMode(i, INPUT);
+    }
+    //pinMode(SPEAKER,OUTPUT);
+
+    for(int i = iStart; i <= iEnd; i++) {
+       digitalWrite(i, LOW);
+    }
+    //digitalWrite(SPEAKER,LOW);
+}
+
+void sound(uint8_t note_index) {
+    for(int i = 0; i < 100; i++) {
+        for(int i = iStart; i <= iEnd; i++) {
+           digitalWrite(i, HIGH);
+        }
+        //digitalWrite(SPEAKER,HIGH);
+        delayMicroseconds(BassTab[note_index]);
+        
+        for(int i = iStart; i <= iEnd; i++) {
+           digitalWrite(i, LOW);
+        }
+        //digitalWrite(SPEAKER,LOW);
+        delayMicroseconds(BassTab[note_index]);
+    }
 }
